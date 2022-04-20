25ff519b5cc7c90e545f81343ebd00017bec99f4
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2022-04-20 21:43:49 +0300

    Разбил код на функции

--- main.py
+++ main.py
@@ -11,24 +11,20 @@ def load_image(url, path, payload=None):
     with open(path, 'wb') as file:
         file.write(response.content)
 
-
-if __name__ == "__main__":
-    load_dotenv()
-
-    #Скачивание комикса
+def load_comics(path):
     response = requests.get(f"https://xkcd.com/{random.randint(0, 2609)}/info.0.json")
     response.raise_for_status()
-    load_image(response.json()["img"], "comics.jpg")
+    load_image(response.json()["img"], path)
     message = response.json()["alt"]
-    
-    
-    #Получение адреса для загрузки фото
+    return message
+
+def get_url_to_load(acess_token, group_id):
     payload = {
-        "group_id": int(os.environ['GROUP_ID']),
+        "group_id": group_id,
         "v": 5.131,
     }
     head = {
-        "Authorization": f"Bearer {os.environ['ACESS_TOKEN']}"
+        "Authorization": f"Bearer {acess_token}"
     }
     response = requests.get(
         "https://api.vk.com/method/photos.getWallUploadServer",
@@ -37,9 +33,10 @@ if __name__ == "__main__":
     )
     response.raise_for_status()
     upload_url = response.json()["response"]["upload_url"]
-    
-    #Загрузка на сервер
-    with open('comics.jpg', 'rb') as file:
+    return upload_url
+
+def load_to_server(image_path, upload_url):
+    with open(image_path, 'rb') as file:
         files = {
             'photo': file,
         }
@@ -48,10 +45,14 @@ if __name__ == "__main__":
         server = response.json()["server"]
         hash = response.json()["hash"]
         photo = response.json()["photo"]
-    
-    #Загрузка на стену
+    return server, hash, photo
+
+def upload_to_wall(server, hash, photo, acess_token, group_id):
+    head = {
+        "Authorization": f"Bearer {acess_token}"
+    }
     payload = {
-        "group_id": int(os.environ['GROUP_ID']),
+        "group_id": group_id,
         "v": 5.131,
         "server": server,
         "hash": hash,
@@ -65,10 +66,15 @@ if __name__ == "__main__":
     response.raise_for_status()
     owner_id = response.json()["response"][0]["owner_id"]
     media_id = response.json()["response"][0]["id"]
-    
-    #Публикация
+
+    return owner_id, media_id
+
+def publish(owner_id, media_id, message, acess_token, group_id):
+    head = {
+        "Authorization": f"Bearer {acess_token}"
+    }
     payload = {
-        "owner_id": -int(os.environ['GROUP_ID']),
+        "owner_id": -group_id,
         "v": 5.131,
         "attachments": f"photo{owner_id}_{media_id}",
         "message": message
@@ -81,6 +87,31 @@ if __name__ == "__main__":
     )
     response.raise_for_status()
 
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    comics_path = "comics.jpg"
+
+    group_id = int(os.environ['GROUP_ID'])
+    acess_token = os.environ['ACESS_TOKEN']
+
+    #Скачивание комикса
+    message = load_comics(comics_path)
+
+    #Получение адреса для загрузки фото
+    upload_url = get_url_to_load(acess_token, group_id)
+    
+    #Загрузка на сервер
+    server, hash, photo = load_to_server(comics_path, upload_url)
+    
+    #Загрузка на стену
+    owner_id, media_id = upload_to_wall(server, hash, photo, acess_token, group_id)
+    
+    #Публикация
+    publish(owner_id, media_id, message, acess_token, group_id)
+
     
     #Удаление комикса
-    os.remove("comics.jpg")
+    os.remove(comics_path)
+
