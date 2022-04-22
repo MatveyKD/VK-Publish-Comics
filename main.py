5380d4bb5e329e88b53806dde61f111b03d0c568
Matvey256 <97842332+MatveyKD@users.noreply.github.com>
2022-04-22 21:39:10 +0300

    Добавил изменения после ревью

--- main.py
+++ main.py
@@ -5,35 +5,61 @@ import requests
 from dotenv import load_dotenv
 
 
-def load_image(url, path, payload=None):    
+class VK_Error(TypeError):
+    pass
+
+
+def load_image(url, path, payload=None):
     response = requests.get(url, params=payload)
     response.raise_for_status()
     with open(path, 'wb') as file:
         file.write(response.content)
 
-def load_comics(path):
-    response = requests.get(f"https://xkcd.com/{random.randint(0, 2609)}/info.0.json")
+
+def check_vk_errors(response):
+    response = response.json()
+    if "error" in response:
+        raise VK_Error(
+            "error_code: {0}, error_message: {1}".format(
+                response["error"]["error_code"],
+                response["error"]["error_msg"],
+            )
+        )
+
+
+def load_random_comics(path):
+    response = requests.get("https://xkcd.com/info.0.json")
     response.raise_for_status()
-    load_image(response.json()["img"], path)
-    message = response.json()["alt"]
+    all_comics_number = response.json()["num"]
+    
+    response = requests.get(
+        f"https://xkcd.com/{random.randint(0, all_comics_number)}/info.0.json"
+    )
+    response.raise_for_status()
+    response_data = response.json()
+    load_image(response_data["img"], path)
+    message = response_data["alt"]
     return message
+
 
 def get_url_to_load(acess_token, group_id):
     payload = {
         "group_id": group_id,
         "v": 5.131,
     }
-    head = {
+    headers = {
         "Authorization": f"Bearer {acess_token}"
     }
     response = requests.get(
         "https://api.vk.com/method/photos.getWallUploadServer",
         params=payload,
-        headers=head
+        headers=headers
     )
     response.raise_for_status()
+    check_vk_errors(response)
     upload_url = response.json()["response"]["upload_url"]
     return upload_url
+
 
 def load_to_server(image_path, upload_url):
     with open(image_path, 'rb') as file:
@@ -42,35 +68,41 @@ def load_to_server(image_path, upload_url):
         }
         response = requests.post(upload_url, files=files)
         response.raise_for_status()
-        server = response.json()["server"]
-        hash = response.json()["hash"]
-        photo = response.json()["photo"]
-    return server, hash, photo
+        check_vk_errors(response)
+        response_data = response.json()
+        comics_server = response_data["server"]
+        comics_hash = response_data["hash"]
+        comics_photo = response_data["photo"]
+    return comics_server, comics_hash, comics_photo
 
-def upload_to_wall(server, hash, photo, acess_token, group_id):
-    head = {
+
+def upload_to_wall(comics_server, comics_hash, comics_photo, acess_token, group_id):
+    headers = {
         "Authorization": f"Bearer {acess_token}"
     }
     payload = {
         "group_id": group_id,
         "v": 5.131,
-        "server": server,
-        "hash": hash,
-        "photo": photo
+        "server": comics_server,
+        "hash": comics_hash,
+        "photo": comics_photo
     }
     response = requests.get(
         "https://api.vk.com/method/photos.saveWallPhoto",
         params=payload,
-        headers=head
+        headers=headers
     )
     response.raise_for_status()
-    owner_id = response.json()["response"][0]["owner_id"]
-    media_id = response.json()["response"][0]["id"]
+    check_vk_errors(response)
+    response_data = response.json()
+    owner_id = response_data["response"][0]["owner_id"]
+    media_id = response_data["response"][0]["id"]
 
     return owner_id, media_id
 
+
 def publish(owner_id, media_id, message, acess_token, group_id):
-    head = {
+    headers = {
         "Authorization": f"Bearer {acess_token}"
     }
     payload = {
@@ -79,39 +111,50 @@ def publish(owner_id, media_id, message, acess_token, group_id):
         "attachments": f"photo{owner_id}_{media_id}",
         "message": message
     }
-    
+
     response = requests.post(
         "https://api.vk.com/method/wall.post",
         params=payload,
-        headers=head
+        headers=headers
     )
     response.raise_for_status()
+    check_vk_errors(response)
+    print(response.content)
 
 
-if __name__ == "__main__":
-    load_dotenv()
-
+def main():
     comics_path = "comics.jpg"
 
     group_id = int(os.environ['GROUP_ID'])
     acess_token = os.environ['ACESS_TOKEN']
 
-    #Скачивание комикса
-    message = load_comics(comics_path)
+    # Скачивание комикса
+    message = load_random_comics(comics_path)
 
-    #Получение адреса для загрузки фото
+
+    # Получение адреса для загрузки фото
     upload_url = get_url_to_load(acess_token, group_id)
-    
-    #Загрузка на сервер
-    server, hash, photo = load_to_server(comics_path, upload_url)
-    
-    #Загрузка на стену
-    owner_id, media_id = upload_to_wall(server, hash, photo, acess_token, group_id)
-    
-    #Публикация
+
+    # Загрузка на сервер
+    comics_server, comics_hash, comics_photo = load_to_server(comics_path, upload_url)
+
+    # Загрузка на стену
+    owner_id, media_id = upload_to_wall(
+        comics_server,
+        comics_hash,
+        comics_photo,
+        acess_token,
+        group_id
+    )
+
+    # Публикация
     publish(owner_id, media_id, message, acess_token, group_id)
 
-    
-    #Удаление комикса
-    os.remove(comics_path)
 
+if __name__ == "__main__":
+    load_dotenv()
+
+    try:
+        main()
+    finally:
+        os.remove("comics.jpg")
